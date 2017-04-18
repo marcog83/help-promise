3313ebc41aa2d7ff9c5bd635b85a28a071ccd697
marcog83 <marcogobbi0@gmail.com>
2017-04-18 16:59:29 +0200

    first commit

--- help-promise.js
+++ help-promise.js
@@ -2,7 +2,7 @@
  * Created by mgobbi on 10/03/2017.
  */
 
-import compose from "./src/compose";
+import compose from "./internal/_compose";
 import equals from "./src/equals";
 import of from "./src/of";
 import filter from "./src/filter";
@@ -10,17 +10,16 @@ import filter from "./src/filter";
 import fromCallback from "./src/from-callback";
 import {then , map, bind} from "./src/then-map-bind";
 import alt from "./src/alt";
+import ap from "./src/ap";
 import {isZero, zero} from "./src/zero";
-import {concat, concatenating} from "./src/concat";
-
-
+import concat from "./src/concat";
 
 export  {
     compose
     , equals
     , concat
-    , concatenating
     , of
+    , ap
     , fromCallback
     , then
     , bind
